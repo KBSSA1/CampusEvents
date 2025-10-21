947163dae9b6984052b0d5f551d868041a374aa1
KBSSA1 <91287544+KBSSA1@users.noreply.github.com>
2025-10-21 10:49:00 -0300

    DAC - Primeiro commit do projeto

---
+++ src/test/java/br/edu/ifpb/dac/campus_events/CampusEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.campus_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
